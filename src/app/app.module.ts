b38721b8d79ff0438b0030bcb3869179c7ab6157
jopwork <jopwork@gmail.com>
2018-01-26 23:13:24 +0800

    New my-button component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { MyButtonComponent } from './my-button/my-button.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyButtonComponent
   ],
   imports: [
     BrowserModule
